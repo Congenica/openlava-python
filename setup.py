28849ce0c881359d5b78b05402414af190604381
ah19 <alex.hodgkins@congenica.com>
2016-03-17 11:46:12 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -14,75 +14,70 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with openlava-python.  If not, see <http://www.gnu.org/licenses/>.
-import os
+import os, os.path
+import glob
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
-# Find lsbatch
-try:
-    lsfdir=os.environ['LSF_ENVDIR']
-    lsfdir=os.path.join(libdir,"..")
+def find_openlava():
+    if 'LSF_ENVDIR' in os.environ:
+        return os.path.abspath(os.path.join(os.environ['LSF_ENVDIR'], '..'))
 
-except:
-    lsfdir='/opt/openlava'
+    if os.path.exists('/opt/openlava'):
+        return '/opt/openlava'
 
-lsf=os.path.join(lsfdir, "lib", "liblsf.a")
-lsbatch=os.path.join(lsfdir, "lib", "liblsbatch.a")
+    #see if there are any openlava versions in opt
+    folders = glob.glob('/opt/openlava-[0-9]*')
+    if folders:
+        if len(folders) > 1:
+            raise Exception("Multiple openlava installations in /opt!")
+        else:
+            return folders[0]
 
-inc_dir=os.path.join(lsfdir,"include")
-lib_dir=os.path.join(lsfdir,"lib")
+    raise Exception("Can't find open installation under /opt (expecting /opt/openlava-3.2 or similar)")
+
+lsfdir = find_openlava()
+
+lsf = os.path.join(lsfdir, "lib", "liblsf.a")
+lsbatch = os.path.join(lsfdir, "lib", "liblsbatch.a")
+
+inc_dir = os.path.join(lsfdir,"include")
+lib_dir = os.path.join(lsfdir,"lib")
 
 if not os.path.exists(lsf):
-    raise ValueError("Cannot find liblsf.a")
+    raise ValueError("Cannot find liblsf.a ({} does not exist)".format(lsf))
 if not os.path.exists(lsbatch):
-    raise ValueError("Cannot find lsbatch.a")
+    raise ValueError("Cannot find lsbatch.a ({} does not exist)".format(lsbatch))
 
-
-def scandir(dir, files=[]):
-    for file in os.listdir(dir):
-        path = os.path.join(dir, file)
-        if os.path.isfile(path) and path.endswith(".pyx"):
-            files.append(path.replace(os.path.sep, ".")[:-4])
-        elif os.path.isdir(path):
-            scandir(path, files)
-    return files
-
-def makeExtension(extName):
-    extPath = extName.replace(".", os.path.sep)+".pyx"
-    return Extension(
-            extName,
-            [extPath],
-            extra_compile_args = ["-O3", "-Wall"],
-            extra_link_args = ['-g'],
-            extra_objects=[lsf, lsbatch],
-            libraries=['lsf','lsbatch','nsl'],
-            include_dirs=[inc_dir,"."],
-            library_dirs=[lib_dir],
-            )
-
-
-
+extensions = [
+    Extension(
+        "*", ["openlava/*.pyx"],
+        extra_compile_args = ["-O3", "-Wall"],
+        extra_link_args    = ['-g'],
+        extra_objects      = [lsf, lsbatch],
+        libraries          = ['lsf','lsbatch','nsl'],
+        include_dirs       = [inc_dir,"."],
+        library_dirs       = [lib_dir],
+    )
+]
 
 setup(
-    extra_compile_args=["-g"],
-    name="openlava-bindings",
-    version="1.0",
-    description="Bindings for OpenLava",
-    author="David Irvine",
-    author_email="irvined@gmail.com",
-    url="https://github.com/irvined1982/openlava-python",
-    license="GPL 3",
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [makeExtension(name) for name in scandir("openlava")],
-    packages=['openlava'],
-    classifiers=[
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.7',
-            'Intended Audience :: Science/Research',
-            'Intended Audience :: System Administrators',
-            'Topic :: Scientific/Engineering',
-            ],
+    name         = "openlava-bindings",
+    version      = "1.0",
+    description  = "Bindings for OpenLava",
+    author       = "David Irvine",
+    author_email = "irvined@gmail.com",
+    url          = "https://github.com/irvined1982/openlava-python",
+    license      = "GPL 3",
+    ext_modules  = cythonize(extensions),
+    packages     = ['openlava'],
+    classifiers  = [
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'Topic :: Scientific/Engineering',
+    ],
 )
-
