e235071b4576e58b9ca34cbe285b2b53370aa662
ah19 <alex.hodgkins@congenica.com>
2016-03-21 17:57:22 +0000

    Add test suite

--- setup.py
+++ setup.py
@@ -20,26 +20,12 @@
 
 import os, os.path
 import glob
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-def find_openlava():
-    if 'LSF_ENVDIR' in os.environ:
-        return os.path.abspath(os.path.join(os.environ['LSF_ENVDIR'], '..'))
-
-    if os.path.exists('/opt/openlava'):
-        return '/opt/openlava'
-
-    #see if there are any openlava versions in opt
-    folders = glob.glob('/opt/openlava-[0-9]*')
-    if folders:
-        if len(folders) > 1:
-            raise Exception("Multiple openlava installations in /opt!")
-        else:
-            return folders[0]
-
-    raise Exception("Can't find open installation under /opt (expecting /opt/openlava-3.2 or similar)")
+from openlava.utils import find_openlava
 
 lsfdir = find_openlava()
 
@@ -75,6 +61,7 @@ setup(
     url          = "https://github.com/irvined1982/openlava-python",
     license      = "GPL 3",
     ext_modules  = cythonize(extensions),
+    test_suite   = 'tests.test.suite',
     packages     = ['openlava'],
     classifiers  = [
         'Programming Language :: Python',
